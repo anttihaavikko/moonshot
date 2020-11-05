019ea97b28d506ad183a7984fd3b8f93756f7d02
Antti Haavikko <antti.haavikko@focus.fi>
2020-11-05 18:57:16 +0200

    Remove debug logs

--- Assets/Scripts/KillLevel.cs
+++ Assets/Scripts/KillLevel.cs
@@ -12,8 +12,6 @@ public class KillLevel : Level
 
     public override void CheckEnd(float time = 0)
     {
-        print(targets.Count);
-        targets.ForEach(e => print(e.hp));
         if (targets.All(e => e.hp <= 0))
         {
             Complete();
